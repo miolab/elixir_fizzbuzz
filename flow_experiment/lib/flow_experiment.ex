2ec5d9dcd866fbfc20af7c48ab0162a9e65536ba
im <poson_hemp@yahoo.co.jp>
2020-05-29 19:52:00 +0900

    mix init

---
+++ flow_experiment/lib/flow_experiment.ex
@@ -0,0 +1,18 @@
+defmodule FlowExperiment do
+  @moduledoc """
+  Documentation for FlowExperiment.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FlowExperiment.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ flow_experiment/test/flow_experiment_test.exs
@@ -0,0 +1,8 @@
+defmodule FlowExperimentTest do
+  use ExUnit.Case
+  doctest FlowExperiment
+
+  test "greets the world" do
+    assert FlowExperiment.hello() == :world
+  end
+end
